3d7350522f36396b6b161329279e828960294c87
Matteu <matteugt@gmail.com>
2025-02-06 01:32:15 +0800

    NUKE NEXT

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="display is-lg">Display Large</h1>
+  <h1 class="display is-md">Display Medium</h1>
+  <h1 class="display is-sm">Display Small</h1>
+
+  <h2 class="headline is-lg">Headline Large</h2>
+  <h2 class="headline is-md">Headline Medium</h2>
+  <h2 class="headline is-sm">Headline Small</h2>
+
+  <h3 class="title is-lg">Title Large</h3>
+  <h3 class="title is-md">Title Medium</h3>
+  <h3 class="title is-sm">Title Small</h3>
+
+  <p class="is-lg">Body Large</p>
+  <p class="is-md">Body Medium</p>
+  <p class="is-sm">Body Small</p>
+</template>
+
+<style scoped>
+
+</style>
